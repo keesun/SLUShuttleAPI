ebd8247b9c7953ca8866c4ec66c533fec7298f13
Keesun Baik <whiteship@epril.com>
2016-03-06 22:19:25 -0800

    Add shuttle 12

--- src/main/java/me/whiteship/service/DataInitializer.java
+++ src/main/java/me/whiteship/service/DataInitializer.java
@@ -1,8 +1,6 @@
 package me.whiteship.service;
 
-import me.whiteship.domain.Building;
-import me.whiteship.domain.Location;
-import me.whiteship.domain.Station;
+import me.whiteship.domain.*;
 import me.whiteship.repository.BuildingRepository;
 import me.whiteship.repository.ShuttleRepository;
 import me.whiteship.repository.StationRepository;
@@ -10,7 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.time.LocalTime;
 import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
 /**
  * @author Keesun Baik
@@ -41,11 +43,47 @@ public class DataInitializer {
                 .build();
         buildingRepository.save(day1North);
 
+        Building blackfoot = Building.builder()
+                .location(Location.builder().longitude(47.6156899f).latitude(-122.3381664f).build())
+                .name("Blackfoot")
+                .build();
+        buildingRepository.save(blackfoot);
+
         Station day1NorthStation = Station.builder()
                 .name("Day 1 North")
                 .connectedBuildings(Arrays.asList(day1North))
                 .build();
-        stationRepository.save(day1NorthStation);
+        day1NorthStation = stationRepository.save(day1NorthStation);
+
+        Station blackfootStation = Station.builder()
+                .name("Blackfoot")
+                .build();
+        stationRepository.save(blackfootStation);
+
+        Calendar departingCal = Calendar.getInstance();
+        departingCal.set(Calendar.HOUR_OF_DAY, 10);
+        departingCal.set(Calendar.MINUTE, 0);
+        departingCal.set(Calendar.SECOND, 0);
+        departingCal.set(Calendar.MILLISECOND, 0);
+        Date departingTime = departingCal.getTime();
+
+        Calendar arrivingCal = Calendar.getInstance();
+        arrivingCal.set(Calendar.HOUR_OF_DAY, 10);
+        arrivingCal.set(Calendar.MINUTE, 5);
+        arrivingCal.set(Calendar.SECOND, 0);
+        arrivingCal.set(Calendar.MILLISECOND, 0);
+        Date arrivingTime = arrivingCal.getTime();
+
+        Shuttle shuttle12 = Shuttle.builder()
+                .number(12)
+                .stations(Arrays.asList(day1NorthStation, blackfootStation))
+                .schedules(Arrays.asList(Schedule.builder()
+                        .departingStation(day1NorthStation)
+                        .departingTime(departingTime)
+                        .arrivingStation(blackfootStation)
+                        .arrivingTime(arrivingTime).build()))
+                .build();
+        shuttleRepository.save(shuttle12);
     }
 
 }
