b64f2000d7d9da8f1f6d3bcb0ee263fc43045147
Keesun Baik <whiteship@epril.com>
2016-03-19 23:01:54 -0700

    Remove test data from ShuttleTest

--- src/test/java/me/whiteship/domain/ShuttleTest.java
+++ src/test/java/me/whiteship/domain/ShuttleTest.java
@@ -1,6 +1,6 @@
 package me.whiteship.domain;
 
-import org.junit.Before;
+import me.whiteship.shuttle.ShuttleService;
 import org.junit.Test;
 
 import java.time.LocalTime;
@@ -17,43 +17,13 @@ import static org.junit.Assert.*;
  */
 public class ShuttleTest {
 
-    private Station trb;
-    private Station blackfoot;
-    private Station arizona;
-    private Station day1North;
+    private Station trb = ShuttleService.TRB;
+    private Station blackfoot = ShuttleService.BLACKFOOT;
+    private Station arizona = ShuttleService.ARIZONA;
+    private Station day1North = ShuttleService.DAY_1_NORTH;
 
-    private Shuttle route1AM;
-    private Shuttle route1PM;
-
-    @Before
-    public void setUp() {
-        trb = Station.builder().name("TRB").build();
-        blackfoot = Station.builder().name("Blackfoot").build();
-        arizona = Station.builder().name("Arizona").build();
-        day1North = Station.builder().name("Day 1 North").build();
-
-        route1AM = Shuttle.builder()
-                .number(1)
-                .description("TRB - Blackfoot - Day 1 North \n" +
-                        "AM Blackfoot & Arizona call-outs")
-                .stations(new Station[]{trb, blackfoot, arizona, day1North})
-                .callouts(new Boolean[]{false, true, true, false})
-                .build();
-        route1AM.addSchedules(trb, "6:55 AM", "7:35 AM", "8:15 AM", "8:55 AM", "9:35 AM", "10:15 AM", "10:50 AM",
-                "11:25 AM", "12:00 PM", "12:45 PM", "1:20 PM", "1:50 PM");
-        route1AM.addSchedules(day1North, "7:15 AM", "8:00 AM", "8:40 AM", "9:20 AM", "9:55 AM", "10:30 AM",
-                "11:05 AM", "11:40 AM", "12:15 PM", "1:00 PM", "1:35 PM");
-
-        route1PM = Shuttle.builder()
-                .number(1)
-                .description("PM: Day 1 North - TRB - Balckfoot")
-                .stations(new Station[]{day1North, trb, blackfoot})
-                .callouts(new Boolean[]{false, false, false})
-                .build();
-        route1PM.addSchedules(day1North, "2:10 PM", "2:45 PM", "3:25 PM", "4:15 PM", "5:15 PM", "6:05 PM", "6:55 PM");
-        route1PM.addSchedules(trb, "2:25 PM", "3:05 PM", "3:45 PM", "4:35 PM", "5:35 PM", "6:25 PM");
-        route1PM.addSchedules(blackfoot, "2:40 PM", "3:20 PM", "4:05 PM", "5:05 PM", "5:55 PM", "6:45 PM");
-    }
+    private Shuttle route1AM = ShuttleService.ROUTE_1_AM;
+    private Shuttle route1PM = ShuttleService.ROUTE_1_PM;
 
     @Test
     public void testParsingTime() {
@@ -182,7 +152,6 @@ public class ShuttleTest {
         } else {
             assertThat(shuttle.getSchedules().get(station).size(), is(count));
         }
-
     }
 
 }
